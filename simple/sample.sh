7e1ba6a0b604af88d21363c22f2a9c44d8d7d321
Jay Patel <jayeshmpatel@hotmail.com>
2022-10-03 13:14:25 -0400

    add training project

---
+++ simple/sample.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo timedatectl set-timezone "America/New_York"
+
+if [ -f '/usr/bin/apt' ]; then
+  sudo apt update
+  sudo apt install -y tree
+else
+  sudo yum install -y tree
+fi
